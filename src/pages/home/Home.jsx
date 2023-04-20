036fe1aa031dd802421d32d751fe20c580988c0b
kunal-gulati-dev <knlgulati365@gmail.com>
2023-04-20 17:22:38 +0530

    added the trending section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import "./style.scss"
-import HeroBanner from './heroBanner/HeroBanner'
+import React from "react";
+import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 const Home = () => {
-  return (
-    <div className='homepage'>
-      <HeroBanner />
-    </div>
-  )
-}
+    return (
+        <div className="homepage">
+            <HeroBanner />
+            <Trending />
+        </div>
+    );
+};
 
-export default Home
+export default Home;
